f7edd03755890d8e32426b9aa68e680d2cc1551b
PUPVANH <Vanhauwerepepijn@hotmail.com>
2026-09-07 10:53:21 +0200

    SEO update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
@@ -7,4 +8,7 @@ export default defineConfig({
   build: {
     inlineStylesheets: 'auto',
   },
+  integrations: [
+    sitemap(),
+  ],
 });
